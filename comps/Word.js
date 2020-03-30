50d92a157e8dbd1ff8c61c2b37157fec5b2211c5
mgsong <mgsong@jjsoft.kr>
2020-03-30 09:13:56 +0900

    remove log

--- comps/Word.js
+++ comps/Word.js
@@ -1,89 +1,101 @@
-import $m from "../com/util";
-import { reqWord, reqWords, saveWord, wordList, deleteWord } from "../src/restful";
+import $m from '../com/util'
+import {
+  reqWord,
+  reqWords,
+  saveWord,
+  wordList,
+  deleteWord,
+} from '../src/restful'
 
-import { equals, complement } from "ramda"
-import moment from "moment";
-import "./Word.scss";
-import app from "../src/app";
+import { equals, complement } from 'ramda'
+import moment from 'moment'
+import './Word.scss'
+import app from '../src/app'
 
-moment.locale("ko");
-
+moment.locale('ko')
 
 const remove = async (word, dom) => {
-    if (!confirm("삭제합니다")) {
-        return;
-    }
+  if (!confirm('삭제합니다')) {
+    return
+  }
 
-    // 애니메이션 시작
-    //await $m.removeAnimation(dom, 0.2)
-    $m.removeAnimation(dom, 0.2)
+  // 애니메이션 시작
+  //await $m.removeAnimation(dom, 0.2)
+  $m.removeAnimation(dom, 0.2)
 
-    // DB 삭제처리
-    let json = await deleteWord(word.id);
-    if (json.status === "Fail") {
-        $m.cancelRemoveAnimation(dom, 0.2)
-    } else {
-        app.view.Index.setState({ list: app.view.Index.state.list.filter(o => o.id !== word.id) })
-    }
+  // DB 삭제처리
+  let json = await deleteWord(word.id)
+  if (json.status === 'Fail') {
+    $m.cancelRemoveAnimation(dom, 0.2)
+  } else {
+    app.view.Index.setState({
+      list: app.view.Index.state.list.filter(o => o.id !== word.id),
+    })
+  }
 }
 
-
-
 function mouseenter(dom) {
-    dom.style.color = "red";
+  dom.style.color = 'red'
 }
 
 function mouseleave(dom) {
-    dom.style.color = "";
+  dom.style.color = ''
 }
-
 
 class Word extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return complement(equals)(this.props.word, nextProps.word)
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    return complement(equals)(this.props.word, nextProps.word)
+  }
 
-    wordClick(){
-        app.view.Index.setState({word: this.props.word.word}, () => {
-            app.view.Index.resizeInput();
-            app.view.Index.search();
-        })
-    }
+  wordClick() {
+    app.view.Index.setState({ word: this.props.word.word }, () => {
+      app.view.Index.resizeInput()
+      app.view.Index.search()
+    })
+  }
 
+  render() {
+    const { word } = this.props
+    return (
+      <li
+        ref={el => {
+          this.dom = el
+        }}
+      >
+        <div className="word">
+          <div
+            className="value"
+            onClick={this.wordClick.bind(this)}
+            onMouseLeave={e => {
+              e.target.style.color = ''
+            }}
+            onMouseEnter={e => {
+              e.target.style.color = 'green'
+            }}
+          >
+            {word.word} <sup>{word.hit}</sup>
+          </div>
 
-    render() {
-        console.log("Word 렌더링")
-        const { word } = this.props;
-        return (
-            <li ref={el => { this.dom = el }} >
-                <div className="word">
-                    <div className="value" onClick={this.wordClick.bind(this)} onMouseLeave={e => {e.target.style.color=""}} onMouseEnter={e => {e.target.style.color="green"}}>
-                        {word.word} <sup>{word.hit}</sup>
-                    </div>
+          <div className="date">{moment(word.updatedAt).fromNow()}</div>
 
+          <div className="name">{`by ${word.userName}`}</div>
 
-                    <div className="date">
-                        {moment(word.updatedAt).fromNow()}
-                    </div>
-
-                    <div className="name">
-                        {`by ${word.userName}`}
-                    </div>
-
-
-                    <div className="remove" onClick={() => remove(word, this.dom)} onMouseLeave={() => mouseleave(this.dom)} onMouseEnter={() => mouseenter(this.dom)}>
-                        <i className="icon-trash-empty" />
-                    </div>
-
-                </div>
-            </li>
-        )
-    }
+          <div
+            className="remove"
+            onClick={() => remove(word, this.dom)}
+            onMouseLeave={() => mouseleave(this.dom)}
+            onMouseEnter={() => mouseenter(this.dom)}
+          >
+            <i className="icon-trash-empty" />
+          </div>
+        </div>
+      </li>
+    )
+  }
 }
-
 
 export default Word
--- src/app.js
+++ src/app.js
@@ -1,83 +1,86 @@
-import getAuth from "./auth";
-import base64js from "base64-js";
-import { isExpired } from "../src/com/pure";
+import getAuth from './auth'
+import base64js from 'base64-js'
+import { isExpired } from '../src/com/pure'
 import createLogger from 'if-logger'
 import moment from 'moment'
 import cookie from 'cookie'
 
 const app = {
-    user: {},
-    state : {
-        menu: [
-            {
-                label: "내가 찾아본 단어",
-                path: "/"
-            }
-        ],
-        menuIdx: 0
-    },
-    view: {}        // 리액트 컴포넌트
+  user: {},
+  state: {
+    menu: [
+      {
+        label: '내가 찾아본 단어',
+        path: '/',
+      },
+    ],
+    menuIdx: 0,
+  },
+  view: {}, // 리액트 컴포넌트
 }
 
-console.log("@@ process.env.NODE_ENV = " + process.env.NODE_ENV)
+console.log('@@ process.env.NODE_ENV = ' + process.env.NODE_ENV)
 
-if (process.env.NODE_ENV === "production") {
-    app.BACKEND = "https://memword-api.now.sh"
-}else{
-    app.BACKEND = "http://localhost:3030"
+if (process.env.NODE_ENV === 'production') {
+  app.BACKEND = 'https://memword-api.now.sh'
+} else {
+  app.BACKEND = 'http://localhost:3030'
 }
 
 app.auth = getAuth(app)
 
-
-
-
-app.isDesktop = function () {
-    const os = ["win16", "win32", "win64", "mac", "macintel"];
-    return global.navigator && os.includes(global.navigator.platform.toLowerCase());
+app.isDesktop = function() {
+  const os = ['win16', 'win32', 'win64', 'mac', 'macintel']
+  return (
+    global.navigator && os.includes(global.navigator.platform.toLowerCase())
+  )
 }
 
-app.isMobileChrome = function () {
-    return !app.isDesktop() && global.navigator && global.navigator.userAgent.includes("Chrome");
+app.isMobileChrome = function() {
+  return (
+    !app.isDesktop() &&
+    global.navigator &&
+    global.navigator.userAgent.includes('Chrome')
+  )
 }
 
 app.Base64Encode = (str, encoding = 'utf-8') => {
-    var bytes = new (TextEncoder || TextEncoderLite)(encoding).encode(str);
-    return base64js.fromByteArray(bytes);
+  var bytes = new (TextEncoder || TextEncoderLite)(encoding).encode(str)
+  return base64js.fromByteArray(bytes)
 }
 
 app.Base64Decode = (str, encoding = 'utf-8') => {
-    var bytes = base64js.toByteArray(str);
-    return new (TextDecoder || TextDecoderLite)(encoding).decode(bytes);
+  var bytes = base64js.toByteArray(str)
+  return new (TextDecoder || TextDecoderLite)(encoding).decode(bytes)
 }
 
-app.getUser = (req) => {
-    try {
-        let userStr;
-        if (req) {
-            const cookies = cookie.parse(req.headers.cookie)
-            userStr = Buffer.from(cookies.user || "", 'base64').toString('utf8');
-        } else {
-            userStr = global.sessionStorage.getItem("user");
-        }
-
-        if (userStr) {
-            let user = JSON.parse(userStr);
-            if (isExpired(user.exp * 1000)){
-                app.logger.verbose("[getInitialProps] 로그인 실패 : Token is expired")
-                return {};
-            } else {
-                app.logger.verbose("[getInitialProps] 로그인 성공", user)
-                return user;
-            }
-        } else {
-            app.logger.verbose("[getInitialProps] 로그인 실패 : user 정보 없음");
-            return {};
-        }
-    } catch (e) {
-        app.logger.error(e)
-        return {};
+app.getUser = req => {
+  try {
+    let userStr
+    if (req) {
+      const cookies = cookie.parse(req.headers.cookie)
+      userStr = Buffer.from(cookies.user || '', 'base64').toString('utf8')
+    } else {
+      userStr = global.sessionStorage.getItem('user')
     }
+
+    if (userStr) {
+      let user = JSON.parse(userStr)
+      if (isExpired(user.exp * 1000)) {
+        app.logger.verbose('[getInitialProps] 로그인 실패 : Token is expired')
+        return {}
+      } else {
+        app.logger.verbose('[getInitialProps] 로그인 성공')
+        return user
+      }
+    } else {
+      app.logger.verbose('[getInitialProps] 로그인 실패 : user 정보 없음')
+      return {}
+    }
+  } catch (e) {
+    app.logger.error(e)
+    return {}
+  }
 }
 
 app.logger = createLogger({
@@ -90,5 +93,5 @@ app.logger = createLogger({
   ],
 })
 
-global.app = app;
-export default app;
+global.app = app
+export default app
