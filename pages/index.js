1f211596f28a2bd2002f0bd7a34eda89c9abf3b7
mgsong <mgsong@jjsoft.kr>
2020-03-30 09:15:34 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -1,197 +1,184 @@
-import { reqWord, reqWords, saveWord, wordList } from "../src/restful";
-import app from "../src/app";
+import { reqWord, reqWords, saveWord, wordList } from '../src/restful'
+import app from '../src/app'
 // import $m from "../com/util";
 import { withRouter } from 'next/router'
-import { observable, reaction, decorate } from "mobx";
-const R = require("ramda");
-global.R = R;
-import Word from "../comps/Word";
+import { observable, reaction, decorate } from 'mobx'
+const R = require('ramda')
+global.R = R
+import Word from '../comps/Word'
 import { Facebook } from 'react-content-loader'
-import Layout from "../comps/Layout";
+import Layout from '../comps/Layout'
 
-import "./index.scss"
+import './index.scss'
 
 class Index extends React.Component {
-    constructor(props) {
-        console.log("Index 생성자 호출")
-        super(props);
+  constructor(props) {
+    console.log('Index 생성자 호출')
+    super(props)
 
-        this.state = {
-            word: "",
-            result: "",
-            loading: false,
-            list: this.props.list || []
-        }
-
-        app.state.menuIdx = props.menuIdx;
-
-        if (props.user && props.user.id) {
-            app.state.userID = props.user.id;
-            app.user = props.user;
-            global.sessionStorage && global.sessionStorage.setItem("user", JSON.stringify(app.user))
-        }
-
-
-        // 변이를 추적할 상태 지정
-        decorate(this, { state: observable });  // or this.state = observable(this.state);
-
-        // 변화에 따른 효과를 정의
-        reaction(() => this.state.word, this.resizeInput);
-
-        app.view.Index = this;
-        app.router = this.props.router;
+    this.state = {
+      word: '',
+      result: '',
+      loading: false,
+      list: this.props.list || [],
     }
 
-    handleChange(e) {
-        this.setState({ word: e.target.value })
+    app.state.menuIdx = props.menuIdx
+
+    if (props.user && props.user.id) {
+      app.state.userID = props.user.id
+      app.user = props.user
+      global.sessionStorage &&
+        global.sessionStorage.setItem('user', JSON.stringify(app.user))
     }
 
+    // 변이를 추적할 상태 지정
+    decorate(this, { state: observable }) // or this.state = observable(this.state);
 
-    resizeInput = () => {
-        console.log("resizeInput 호출")
-        let ta = this.input;
-        ta.style.height = "";
+    // 변화에 따른 효과를 정의
+    reaction(() => this.state.word, this.resizeInput)
 
-        if (ta.scrollHeight > ta.offsetHeight) {
-            ta.style.height = ta.scrollHeight + "px"
-        }
+    app.view.Index = this
+    app.router = this.props.router
+  }
 
-        if (this.state.word === "") {
-            ta.style.height = "";
-        }
+  handleChange(e) {
+    this.setState({ word: e.target.value })
+  }
+
+  resizeInput = () => {
+    console.log('resizeInput 호출')
+    let ta = this.input
+    ta.style.height = ''
+
+    if (ta.scrollHeight > ta.offsetHeight) {
+      ta.style.height = ta.scrollHeight + 'px'
     }
 
+    if (this.state.word === '') {
+      ta.style.height = ''
+    }
+  }
 
-    async search() {
-        if (this.state.word === "") {
-            //alert("단어나 문장을 입력하세요");
-            return;
-        }
-
-        const isSentence = R.pipe(
-            R.trim,
-            R.split(" "),
-            R.length,
-            R.gt(R.__, 1)
-        )
-
-        const getResult = R.ifElse(
-            isSentence,
-            reqWords,
-            reqWord
-        )
-
-        this.setState({ loading: true });
-
-        R.pipeP(
-            getResult,
-            ({result}) => {
-                this.setState({
-                    result,
-                    loading: false
-                })
-            },
-        )(this.state.word)
-
-
-        R.pipe(
-            R.ifElse(
-                R.complement(isSentence),
-                R.toLower,
-                R.identity
-            ),
-            saveWord
-        )(this.state.word)
-
-        // saveWord(this.state.word);
+  async search() {
+    if (this.state.word === '') {
+      //alert("단어나 문장을 입력하세요");
+      return
     }
 
+    const isSentence = R.pipe(R.trim, R.split(' '), R.length, R.gt(R.__, 1))
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return R.complement(R.equals)(this.state, nextState)
+    const getResult = R.ifElse(isSentence, reqWords, reqWord)
+
+    this.setState({ loading: true })
+
+    R.pipeP(getResult, ({ result }) => {
+      this.setState({
+        result,
+        loading: false,
+      })
+    })(this.state.word)
+
+    R.pipe(
+      R.ifElse(R.complement(isSentence), R.toLower, R.identity),
+      saveWord,
+    )(this.state.word)
+
+    // saveWord(this.state.word);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return R.complement(R.equals)(this.state, nextState)
+  }
+
+  static async getInitialProps({ req, asPath }) {
+    app.logger.verbose('Index의 getInitialProps 호출')
+    let user = app.getUser(req)
+
+    app.user.token = user.token
+
+    let { list } = await wordList()
+    // app.logger.verbose('list:', list)
+
+    app.state.menuIdx = app.state.menu.findIndex(m => m.path === asPath)
+
+    if (req) {
+      // 서버에서
+    } else {
+      // 클라이언트에서
+      //app.view.Index.state.list = list;
     }
+    return { list, user, menuIdx: app.state.menuIdx }
+  }
 
+  componentDidMount() {
+    this.input.focus()
+  }
 
-    static async getInitialProps({ req, asPath }) {
-        app.logger.verbose("Index의 getInitialProps 호출")
-        let user = app.getUser(req);
-        
-        app.user.token = user.token;
+  initWord = async () => {
+    const { list } = await wordList()
+    this.setState({ word: '', result: '', list })
+    this.input.focus()
+  }
 
-        let { list } = await wordList();
-        app.logger.verbose('list:', list)
-        
-        app.state.menuIdx = app.state.menu.findIndex(m => m.path === asPath);
+  render() {
+    console.log('Index 렌더링')
 
-        if (req) {
-            // 서버에서
-        } else {
-            // 클라이언트에서
-            //app.view.Index.state.list = list;
-        }
-        return { list, user, menuIdx: app.state.menuIdx}
-    }
+    const res = this.state.result ? (
+      <React.Fragment>
+        <div className="title2">검색 결과</div>
+        <div
+          className="result"
+          dangerouslySetInnerHTML={{ __html: this.state.result }}
+        ></div>
+      </React.Fragment>
+    ) : (
+      <Facebook />
+    )
 
+    const finded = (
+      <React.Fragment>
+        <div className="title2">
+          {app.auth.isLogin()
+            ? app.state.menu[app.state.menuIdx].label
+            : '로그인이 필요합니다 '}
+        </div>
+        <div className="history">
+          <ul>
+            {this.state.list.map(o => (
+              <Word key={o.id} word={o} />
+            ))}
+          </ul>
+        </div>
+      </React.Fragment>
+    )
 
-    componentDidMount() {
-        this.input.focus();
-    }
-
-    initWord = async () => {
-        const { list } = await wordList();
-        this.setState({ word: "", result: "", list })
-        this.input.focus();
-    }
-
-    render() {
-        console.log("Index 렌더링")
-
-        const res = this.state.result
-            ?
-            <React.Fragment>
-                <div className="title2">검색 결과</div>
-                <div className="result" dangerouslySetInnerHTML={{ __html: this.state.result }}>
-                </div>
-            </React.Fragment>
-            :
-            <Facebook />
-
-        const finded = <React.Fragment>
-            <div className="title2">{app.auth.isLogin() ? app.state.menu[app.state.menuIdx].label : "로그인이 필요합니다 " }</div>
-            <div className="history">
-                <ul>
-                    {
-                        this.state.list.map(o => <Word key={o.id} word={o} />)
-                    }
-                </ul>
+    return (
+      <Layout>
+        <div className="lower">
+          <div className="wrapper">
+            <div className="title">단어 및 문장을 검색해 보세요</div>
+            <div className="word">
+              <textarea
+                value={this.state.word}
+                ref={ele => {
+                  this.input = ele
+                }}
+                onChange={this.handleChange.bind(this)}
+              ></textarea>
+              {this.state.word && (
+                <div className="icon-cancel delete" onClick={this.initWord} />
+              )}
+              <button onClick={this.search.bind(this)}>검색</button>
             </div>
-        </React.Fragment>
-
-
-        return (
-            <Layout>
-                <div className="lower">
-                    <div className="wrapper">
-                        <div className="title">
-                            단어 및 문장을 검색해 보세요
-                        </div>
-                        <div className="word">
-                            <textarea value={this.state.word} ref={ele => { this.input = ele }} onChange={this.handleChange.bind(this)}></textarea>
-                            {this.state.word &&
-                                <div className="icon-cancel delete" onClick={this.initWord} />
-                            }
-                            <button onClick={this.search.bind(this)}>검색</button>
-                        </div>
-                        <div className="resultWrapper">
-                            {
-                                this.state.result === "" && !this.state.loading ? finded : res
-                            }
-                        </div>
-                    </div>
-                </div>
-            </Layout>
-        )
-    }
+            <div className="resultWrapper">
+              {this.state.result === '' && !this.state.loading ? finded : res}
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default withRouter(Index)
